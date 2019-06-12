f4a4976a502443167d7c2e1c83def200e6e49b99
madhucs <madhuwhizz@gmail.com>
2019-06-11 23:48:52 -0500

    small changes4

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,12 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
+echo "\n BEGIN  travis repo slug data \n "
+
+echo "\n \n "
+echo $TRAVIS_REPO_SLUG:$TAG
+
+echo "\n END OF  travis repo slug data \n "
+
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
-echo $TRAVIS_REPO_SLUG:$TAG
